e2c641bcf660340c7529690b0b7205ba60c3f747
hungdomanh <25021784@vnu.edu.vn>
2026-04-09 18:31:56 +0700

    Thêm các file +)AuctionService: tạo phiên đấu giá, đặt giá(có kiểm tra logic), xử lý đấu giá đồng thời. +)AuctionServiceTest: Kiểm thử AuctionService +)Sửa lại 1 vài thuộc tính trong phần shared

--- auctionSystem/src/main/java/com/auction/demo/client/AppLauncher.java
+++ auctionSystem/src/main/java/com/auction/demo/client/AppLauncher.java
@@ -2,6 +2,6 @@ package com.auction.demo.client;
 
 public class AppLauncher {
     public static void main(String[] args) {
-        MainApp.main(args);
+        ClientApp.main(args);
     }
 }
---
+++ auctionSystem/src/main/java/com/auction/demo/client/ClientApp.java
@@ -0,0 +1,39 @@
+package com.auction.demo.client;
+
+import com.auction.demo.client.dao.UserDao;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.sql.SQLException;
+
+public class ClientApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        initializeDatabase();
+
+        // Load Login.fxml
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Login.fxml"));
+        AnchorPane loginView = loader.load();
+
+        // Tạo scene
+        Scene scene = new Scene(loginView, 600, 400);
+
+        // Setup stage
+        primaryStage.setTitle("Auction System - Client");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    private void initializeDatabase() throws SQLException {
+        UserDao userDao = new UserDao();
+        userDao.initializeDatabase();
+    }
+}
