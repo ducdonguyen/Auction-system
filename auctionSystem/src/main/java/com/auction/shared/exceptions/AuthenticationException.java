84f2de5212056137e6d61ca2e14ca72cb51fa0ca
DucDoNguyen <ducdo252322@gmail.com>
2026-04-16 18:49:17 +0700

    Sửa code của Hùng ngáo

--- auctionSystem/src/main/java/com/auction/shared/exceptions/AuthenticationException.java
+++ auctionSystem/src/main/java/com/auction/shared/exceptions/AuthenticationException.java
@@ -1,6 +1,6 @@
 package com.auction.shared.exceptions;
 
-public class AuthenticationException extends Exception {
+public class AuthenticationException extends RuntimeException {
     public AuthenticationException(String message) {
         super(message);
     }
