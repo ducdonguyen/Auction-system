445eaa5be71fdf36bfdecb2ec550b67a5f7a7abb
DucDoNguyen <ducdo252322@gmail.com>
2026-05-29 09:23:21 +0700

    Xóa các class không cần thiết

--- auctionSystem/src/main/java/com/auction/server/util/IScheduler.java
+++
@@ -1,7 +0,0 @@
-package com.auction.server.util;
-
-import java.util.concurrent.TimeUnit;
-
-public interface IScheduler {
-    void scheduleAtFixedRate(Runnable command, long initialDelay, long period, TimeUnit unit);
-}
